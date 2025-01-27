aac4d9c714700fbf28100fc861ba193f19fff41d
Antonio Fregoso <antoniofregoso@protonmail.com>
2025-01-27 00:20:24 -0600

    [ADD] pages

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+  // BURGERS
+  const $burgers = document.querySelectorAll(".js-burger");
+
+  $burgers.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetID = el.dataset.target;
+      const $target = document.getElementById(targetID);
+      el.classList.toggle("is-active");
+      $target.classList.toggle("is-active");
+      e.stopPropagation();
+    });
+  });
+
