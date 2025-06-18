dd3eb3ab847b863f731fe41a35b2f12a1f755721
Antonio Fregoso <antoniofregoso@protonmail.com>
2025-06-17 23:41:10 -0600

    [ADD] cookie consent modal

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -4,12 +4,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
 class App {
 
+  cookieConsent = {
+
+        categories: {
+            necessary: {
+                enabled: true,  // this category is enabled by default
+                readOnly: true  // this category cannot be disabled
+            },
+            analytics: {}
+        },
+
+        language: {
+            default: 'en',
+            translations: {
+                en: {
+                    consentModal: {
+                        title: 'We use cookies',
+                        description: 'Cookie modal description',
+                        acceptAllBtn: 'Accept all',
+                        acceptNecessaryBtn: 'Reject all',
+                        showPreferencesBtn: 'Manage Individual preferences'
+                    },
+                    preferencesModal: {
+                        title: 'Manage cookie preferences',
+                        acceptAllBtn: 'Accept all',
+                        acceptNecessaryBtn: 'Reject all',
+                        savePreferencesBtn: 'Accept current selection',
+                        closeIconLabel: 'Close modal',
+                        sections: [
+                            {
+                                title: 'Somebody said ... cookies?',
+                                description: 'I want one!'
+                            },
+                            {
+                                title: 'Strictly Necessary cookies',
+                                description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+
+                                //this field will generate a toggle linked to the 'necessary' category
+                                linkedCategory: 'necessary'
+                            },
+                            {
+                                title: 'Performance and Analytics',
+                                description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
+                                linkedCategory: 'analytics'
+                            },
+                            {
+                                title: 'More information',
+                                description: 'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }
+  
   constructor(){
     this.setTheme();
     this.addEvents();
     this.setupSliders();
     this.detectMenuActive();    
     this.whithAnimations();
+    CookieConsent.run(this.cookieConsent);
   }
 
   handleEvent(e){
@@ -38,6 +94,7 @@ class App {
           document.getElementById('themes').innerHTML = '<i class="fas fa-moon">'
           document.getElementById('themes').parentNode.classList.toggle('is-active');
           document.documentElement.setAttribute('data-theme', 'dark');
+          document.documentElement.classList.add('cc--darkmode');
           localStorage.setItem('theme', 'dark');
           break;
         case 'system-theme':
@@ -73,6 +130,7 @@ class App {
       document.documentElement.setAttribute('data-theme', 'dark');
       document.getElementById('themes').innerHTML = '<i class="fas fa-moon">';
       document.getElementById('dark-theme').classList.add('is-active');
+      document.documentElement.classList.add('cc--darkmode');
     }else if (theme==='light'){
       document.documentElement.setAttribute('data-theme', 'light');
       document.getElementById('themes').innerHTML = '<i class="fas fa-sun">';
